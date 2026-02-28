d8871b998b97fc4073f401a31520652ea5474d80
Stefano Cattonar <cesarstef@outlook.com>
2026-02-28 17:57:08 +0100

    Fix

--- include/stencil_parallel.h
+++ include/stencil_parallel.h
@@ -335,8 +335,9 @@ inline int update_plane ( const int      periodic,
     double * restrict new = newplane->data;
     
     #pragma omp parallel for schedule(static) collapse(2)// static schedule for regular grid
-    for (uint j = 1; j <= ysize; j++)
-        for ( uint i = 1; i <= xsize; i++)
+    {
+        for (uint j = 1; j <= ysize; j++){
+            for ( uint i = 1; i <= xsize; i++)
             {
                 
                 // NOTE: (i-1,j), (i+1,j), (i,j-1) and (i,j+1) always exist even
@@ -351,10 +352,11 @@ inline int update_plane ( const int      periodic,
                 //
                 new[ IDX(i,j) ] =
                     old[ IDX(i,j) ] / 2.0 + ( old[IDX(i-1, j)] + old[IDX(i+1, j)] +
-                                              old[IDX(i, j-1)] + old[IDX(i, j+1)] ) /4.0 / 2.0;
+                                            old[IDX(i, j-1)] + old[IDX(i, j+1)] ) /4.0 / 2.0;
                 
             }
-
+        }
+    }
     if ( periodic )
         {
             if ( N[_x_] == 1 )
@@ -403,12 +405,13 @@ inline int get_total_energy( plane_t *plane,
     //       (i)  manually unroll the loop
     //       (ii) ask the compiler to do it
     // for instance
-    // #pragma GCC unroll 4
-    #pragma omp parallel for schedule(static) collapse(2) reduction(+:totenergy)
+    #pragma GCC unroll 4
+    #pragma omp for schedule(static) collapse(2) reduction(+:totenergy)
+    {
         for ( int j = 1; j <= ysize; j++ )
             for ( int i = 1; i <= xsize; i++ )
                 totenergy += data[ IDX(i, j) ];
-
+    }
     
    #undef IDX
 
